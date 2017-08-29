d789a4d84c13080fe87cf9297636d07bf1ded565
Efi MK <efi.merdler@gmail.com>
2017-08-29 11:28:09 +0300

    Added support for no location

--- android-easylocation/src/main/java/com/akhgupta/easylocation/AppConstants.java
+++ android-easylocation/src/main/java/com/akhgupta/easylocation/AppConstants.java
@@ -6,4 +6,6 @@ class AppConstants {
     public static final String ACTION_LOCATION_FETCH_START = "location.fetch.start";
     public static final String ACTION_LOCATION_FETCH_STOP = "location.fetch.stop";
     public static final String INTENT_LOCATION_RECEIVED = "intent.location.received";
+    public static final String INTENT_NO_LOCATION_RECEIVED = "intent.no.location.received";
+
 }
